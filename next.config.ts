3efd9f833e9b82611c99f97aee6985d1fc5ce41a
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-13 03:22:37 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // Permet au build de production de réussir même avec des erreurs ESLint
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
