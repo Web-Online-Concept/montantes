fb4798bf8769ad06ea8bf52d7c7a134c695e380a
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-14 13:16:30 +0200

    Update CarteMontante.tsx

--- components/CarteMontante.tsx
+++ components/CarteMontante.tsx
@@ -24,7 +24,7 @@ export default function CarteMontante({ montante }: CarteMontanteProps) {
       etatConfig = {
         label: 'Arrêtée' as const,
         couleur: '#6b7280',
-        emoji: 'ℹ️' as const
+        emoji: '⏹️' as const
       }
     }
   }
