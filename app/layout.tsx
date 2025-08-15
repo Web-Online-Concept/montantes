da255f0571ecd8c273b29518ed39ea09ecd4cb80
Web-Online-Concept <web.online.concept@gmail.com>
2025-08-15 02:08:14 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,20 +18,8 @@ export const metadata: Metadata = {
     canonical: '/',
   },
   icons: {
-    icon: [
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-      { url: '/favicon.ico', sizes: 'any' }
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' }
-    ],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/logo.png',
-      }
-    ]
+    icon: '/logo.png',
+    apple: '/logo.png',
   },
   manifest: '/manifest.json',
   openGraph: {
